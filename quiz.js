aa5a1298751e8ca2ae62ef2b54ed9c4eb7d32aed
Morchiid Aymane <morxiidbullishly@gmail.com>
2025-01-26 22:13:42 +0100

    modify textContent Feedback

--- quiz.js
+++ quiz.js
@@ -20,7 +20,7 @@ function checkAnswer() {
         feedback.textContent = "Correct! Well done.";
         feedback.style.color = "green";
     } else {
-        feedback.textContent = "";
+        feedback.textContent = "That's incorrect. Try again!";
         feedback.style.color = "red";
     }
 }
